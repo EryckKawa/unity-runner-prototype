d947aeab5489a7a0bdfd1955c97b2d22f88fe45a
Eryck Kawa <okazakiall114@gmail.com>
2024-06-07 16:05:51 -0300

    challenge 3 finished

---
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+	public bool gameOver;
+
+	public float floatForce;
+	private float gravityModifier = 1.5f;
+	private Rigidbody playerRb;
+
+	public ParticleSystem explosionParticle;
+	public ParticleSystem fireworksParticle;
+
+	private AudioSource playerAudio;
+	public AudioClip moneySound;
+	public AudioClip explodeSound;
+	[SerializeField] private AudioClip bounceSound;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Physics.gravity *= gravityModifier;
+
+		playerRb = GetComponent<Rigidbody>();
+		playerAudio = GetComponent<AudioSource>();
+
+		// Apply a small upward force at the start of the game
+		playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// While space is pressed and player is low enough, float up
+		if (Input.GetKey(KeyCode.Space) && !gameOver)
+		{
+			playerRb.AddForce(Vector3.up * floatForce);
+		}
+		
+		LimitUpper();
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Ground"))
+		{
+			playerAudio.PlayOneShot(bounceSound, 1.0f);
+			Bounce();
+		}
+
+		// if player collides with bomb, explode and set gameOver to true
+		else if (other.gameObject.CompareTag("Bomb"))
+		{
+			playerAudio.PlayOneShot(explodeSound, 1.0f);
+			gameOver = true;
+			Debug.Log("Game Over!");
+			Destroy(other.gameObject);
+		} 
+
+		// if player collides with money, fireworks
+		else if (other.gameObject.CompareTag("Money"))
+		{
+			fireworksParticle.Play();
+			playerAudio.PlayOneShot(moneySound, 1.0f);
+			Destroy(other.gameObject);
+
+		}
+
+	}
+	private void LimitUpper()
+	{
+		float limitX = 16;
+		float downForce = 10;
+		
+		if (transform.position.y > limitX)
+		{
+			transform.position = new Vector3(transform.position.x, limitX, transform.position.z);
+			playerRb.AddForce(Vector3.up * - downForce, ForceMode.Impulse);
+		}
+	}
+	
+	private void Bounce()
+	{
+		float limitY = 0;
+		float bounceForce = 10;
+		if (transform.position.y <= limitY)
+		{
+			playerRb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,48 +4,91 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private Rigidbody playerRb;
-	[SerializeField] private float jumpForce;
-	[SerializeField] private float gravityMulti;
-	[SerializeField] private bool isOnGround = true;
-	private bool gameOver = false;
+    // Components
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    
+    // Serialized fields
+    [SerializeField] private ParticleSystem explosionParticle;
+    [SerializeField] private ParticleSystem dirtParticle;
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioClip deathSound;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravityMulti;
+    [SerializeField] private bool isOnGround = true;
 
-	private const string GROUND = "Ground";
-	private const string OBSTACLE = "Obstacle";
+    // Private fields
+    private bool gameOver = false;
 
+    // Constants for tags and animation parameters
+    private const string GROUND = "Ground";
+    private const string OBSTACLE = "Obstacle";
+    private const string JUMP_TRIG = "Jump_trig";
+    private const string DEATH_B = "Death_b";
+    private const string DEATHTYPE_INT = "DeathType_int";
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		playerRb = GetComponent<Rigidbody>();
-		Physics.gravity *= gravityMulti;
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        
+        Physics.gravity *= gravityMulti;
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-		{
-			playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-			isOnGround = false;
-		}
-	}
-	
-	private void OnCollisionEnter(Collision other)
-	{
-		if (other.gameObject.CompareTag(GROUND))
-		{
-			isOnGround = true;
-		}
-		else if (other.gameObject.CompareTag(OBSTACLE))
-		{
-			Debug.Log("Game Over!");
-			gameOver = true;
-		}
-	}
-	
-	public bool GetGameOver()
-	{
-		return gameOver;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            Jump();
+        }
+    }
+    
+    private void Jump()
+    {
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        playerAnim.SetTrigger(JUMP_TRIG);
+        isOnGround = false;
+
+        playerAudio.PlayOneShot(jumpSound);
+        dirtParticle.Stop();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(GROUND))
+        {
+            Land();
+        }
+        else if (other.gameObject.CompareTag(OBSTACLE))
+        {
+            Die();
+        }
+    }
+    
+    private void Land()
+    {
+        isOnGround = true;
+        dirtParticle.Play();
+    }
+
+    private void Die()
+    {
+        playerAnim.SetBool(DEATH_B, true);
+        playerAnim.SetInteger(DEATHTYPE_INT, 1);
+
+        explosionParticle.Play();
+        dirtParticle.Stop();
+        playerAudio.PlayOneShot(deathSound);
+    
+        gameOver = true;
+    }
+
+    public bool GetGameOver()
+    {
+        return gameOver;
+    }
 }
